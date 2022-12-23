e4aea25a537a6a0724d05c9459c93d2bf1ca4599
Saket verma <63857530+saketverma-03@users.noreply.github.com>
2022-12-24 00:14:21 +0530

    CREATED Controller Password.js
    created controllers for passwords routes

---
+++ src/controllers/password.js
@@ -0,0 +1,74 @@
+// const { response } = require("express");
+// const password = require("../models/password");
+const Password = require("../models/password")
+
+
+exports.savePasword = (req, res) => {
+    const passwordItem = new Password({
+        ...req.body,
+        user: req.userId
+    });
+
+    passwordItem.save((e, savedItem) => {
+        if (e) {
+            return res.status(400).json({
+                message: "Password Not Saved",
+                error: e
+            })
+        }
+        else {
+            return res.status(200).json({message:"Pssword saved Successfully"})
+        }
+    })
+}
+
+exports.getAllPassword = (req, res) => {
+    // const Item = new Password()
+    Password.find({ userId: req.userId }, (err, items) => {
+        if (err) {
+            // Handle the error
+            return res.status(500).json({
+                error: err,
+                message: "Can not fetch password pleas try loging in again"
+            });
+        }
+        return res.status(400).json({items,message:"Synced to Database"})
+    })
+}
+
+exports.deleteOne = (req,res) =>  {
+  
+    Password.findByIdAndDelete(req.body._id, (err) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+            message: "Password Deletion Failed! , Pleas Try again later"
+        })
+        } else {
+          res.status(200).json({
+            message: "Password Deleted Successfully"
+          })
+        }
+      });
+}
+
+exports.updateOne = (req,res) => {
+    const id = req.body._id
+    Password.findById(id,(error,item) => {
+        if(error){
+            return res.status(400).json({message:"Password Updation Password",error})
+        }
+        if(!item){
+            return res.status(400).json({message:"item not found",error})
+        }
+        item.productUsername = req.body.productUsername;
+        item.productWebsite = req.body.productWebsite;
+        item.password = req.body.password;
+        item.save((error) => {
+            
+            res.status(200).json({message:"Succesfully Updated"})
+        })
+    });
+   
+    
+}
